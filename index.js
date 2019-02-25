96d0b38835be573337af862ca1bc7446e5bedc5a
timothyjgrant11@gmail.com <timothyjgrant11@gmail.com>
2019-02-25 16:28:08 -0500

    changes

--- index.js
+++ index.js
@@ -1,19 +1,2 @@
-$(document).ready(function() {
-	$(".add").click(addItem);
+$(document).ready(function () {
 
-	// Add item when Enter key is pressed
-	$(".add-item").keydown(function(event) {
-		var keycode = event.keyCode ? event.keyCode : event.which;
-		if(keycode == 13){
-			addItem();
-		}
-	});
-
-	// Remove item from list when Delete is clicked
-	$(".buy-item").on("click", ".delete", function() {
-    	$(this).closest(".to-buy").remove();
-	});
-	
-	$(".purchased").on("click", ".delete", function() {
-    	$(this).closest(".to-buy").remove();
-	});
