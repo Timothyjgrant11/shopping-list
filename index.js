48a8e36deafa4a4296e409790a192f9114543ad1
timothyjgrant11@gmail.com <timothyjgrant11@gmail.com>
2019-02-26 16:30:26 -0500

    final changes

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 
 function createListHtml(value){
-	return `<li><span class="shopping-item">${value}</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>`
+    return `<li>
+        <span class="shopping-item">${value}</span>
+        <div class="shopping-item-controls">
+        <button class="shopping-item-toggle">
+        <span class="button-label">check</span></button>
+        <button class="shopping-item-delete">
+        <span class="button-label">delete</span></button>
+        </div>
+        </li>`
 }
 
 function checkItem(){
